b4c35dbaa88edbe7d1519475751fa22ebb97b26f
vivek <vivekbilla822@gmail.com>
2025-08-02 22:44:35 +0530

    order now

--- src/MockData/recipes.js
+++ src/MockData/recipes.js
@@ -9,13 +9,31 @@ const recipes = [
     id: 2,
     name: "Strawberry Bomboloni",
     price: 100,
-    image: "/images/strawberry.jpg",
+    image: "/images/strawberry.jpeg",
   },
   {
     id: 3,
     name: "Custard Bomboloni",
     price: 95,
     image: "../images/custard.jpg",
+  },
+  {
+    id: 4,
+    name: "Strawberry Bomboloni",
+    price: 100,
+    image: "/images/strawberry.jpeg",
+  },
+  {
+    id: 5,
+    name: "Strawberry Bomboloni",
+    price: 100,
+    image: "/images/strawberry.jpeg",
+  },
+  {
+    id: 6,
+    name: "Strawberry Bomboloni",
+    price: 100,
+    image: "/images/strawberry.jpeg",
   },
 ];
 
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 
 const RecipeCard = ({ name, price, image }) => {
+  const phoneNumber = "918149307910";
+  const message = `Hi, I would like to order:\n${name} - ₹${price}`;
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  // Replace 91XXXXXXXXXX with your real number
+
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden w-full max-w-xs">
-      <img src={image} alt={name} className="w-full h-48 object-cover" />
-      <div className="p-4">
-        <h3 className="text-lg font-bold text-gray-800">{name}</h3>
-        <p className="text-pink-600 font-semibold mb-3">₹{price}</p>
-        <button className="w-full bg-pink-600 hover:bg-pink-700 text-white py-2 rounded-md font-bold transition">
-          Add to Cart
-        </button>
+    <div className="bg-blue-950 rounded-xl overflow-hidden shadow-md hover:shadow-xl transition duration-300 w-full max-w-md">
+      <img
+        src={image}
+        alt={name}
+        className="w-full h-56 object-cover p-2"
+      />
+      <div className="p-6 flex flex-col justify-between h-full">
+        <div>
+          <h3 className="text-xl font-bold text-white mb-2">{name}</h3>
+          <p className="text-lg text-pink-600 font-semibold mb-4">₹{price}</p>
+          <a
+          href={whatsappURL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4 p-2 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md font-semibold text-center transition duration-300"
+        >
+          Order Now
+        </a>
+        </div>
+        
       </div>
     </div>
   );
 };
 
 export default RecipeCard;
+
